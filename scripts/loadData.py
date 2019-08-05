38d76c0a5d36e193aca28d6c35d80a67d77dda38
jh-206 <jhirschi206@gmail.com>
2019-08-04 17:32:59 -0700

    Create loadData script

---
+++ scripts/loadData.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 17:13:48 2019
+
+@author: jhirschi
+"""
+
+import matplotlib
+
+
+datadir = "C:/Users/jhirschi/Documents/Code/Python/seedling_classification/data/raw/v2-plant-seedlings-dataset"
